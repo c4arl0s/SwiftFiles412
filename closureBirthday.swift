ac96dc3635557449d48db281ef235ce78900f35a
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-09-18 22:41:51 -0500

    studying closures

---
+++ closureBirthday.swift
@@ -0,0 +1,5 @@
+let birthday:(String) -> () = { name in
+    print("Happy birthday, \(name)!")
+}
+
+birthday("Carlos")
---
+++ simpleClosure.swift
@@ -0,0 +1,10 @@
+let simpleClosure = {   
+}
+simpleClosure()
+
+// In the above syntax, we have declared a simple closure { } that takes no parameters, 
+// contains no statements and does not return a value. 
+// This closure is assigned to the constant simpleClosure.
+
+// We call the closure as simpleClosure(), but since it doesn't contain any statements, the program does nothing.
+
